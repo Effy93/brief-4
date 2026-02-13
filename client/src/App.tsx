31828466c06a64b0f9786dd64e862f9a08cb89ab
Eva Philippo <philippoeva@gmail.com>
2026-02-13 15:50:16 +0100

    feat: add DisplayArticle component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,10 @@
 import "./App.css";
-import DisplayArticle from "./components/displayArticle/DisplayArticle";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="layout">
-      <section className="left-section">
-        <Home />
-      </section>
-
-      <section className="right-section">
-        <DisplayArticle />
-      </section>
+      <Home />
     </div>
   );
 }
