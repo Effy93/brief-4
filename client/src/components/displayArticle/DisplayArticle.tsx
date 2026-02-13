df17782fc86b8c9ff399a13905ddb145ca6955bb
Alexandre Maunoury <alexandremaunoury@yahoo.fr>
2026-02-13 16:57:26 +0100

    feat: connect home page with components

--- client/src/components/displayArticle/DisplayArticle.tsx
+++ client/src/components/displayArticle/DisplayArticle.tsx
@@ -1,24 +1,22 @@
 import "./displayArticle.css";
 
-interface displayArticle {
+interface DisplayArticle {
   notion: string;
   content: string;
 }
 
-export default function displayArticle() {
+export default function DisplayArticle() {
   return (
     <div className="right-section">
       <button type="button" className="button-random">
-        {" "}
-        Generate random notion{" "}
+        Generate random notion
       </button>
       <div className="container-article">
         <h2> Provider - React </h2>
       </div>
       <p>
-        {" "}
         Le provider c'est le robinet, le consummer (useContext) c'est le verre
-        d'eau{" "}
+        d'eau
       </p>
     </div>
   );
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,30 @@
+import Cards from "../components/card/Cards";
+import DisplayArticle from "../components/displayArticle/DisplayArticle";
+import "./home.css";
+import { BiSearchAlt } from "react-icons/bi";
 export default function Home() {
   return (
-    <div>
+    <section className="section-home">
       <h1>Popularize</h1>
-    </div>
+      <div className="main-container">
+        <div className="left-container">
+          <div className="input-container">
+            <input type="text" placeholder="react,backend,SGBD" />
+            <BiSearchAlt size={35} />
+          </div>
+          <div className="cards-container">
+            <Cards />
+            <Cards />
+            <Cards />
+            <Cards />
+            <Cards />
+            <Cards />
+          </div>
+        </div>
+        <div className="right-container">
+          <DisplayArticle />
+        </div>
+      </div>
+    </section>
   );
 }
