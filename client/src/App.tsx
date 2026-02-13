a2ffbcdc63b9053cb6adebb4c916bb448e104711
Eva Philippo <philippoeva@gmail.com>
2026-02-13 15:43:35 +0100

    feat: add DisplayArticle component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,18 @@
 import "./App.css";
+import DisplayArticle from "./components/displayArticle/DisplayArticle";
 import Home from "./pages/Home";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <div className="layout">
+      <section className="left-section">
+        <Home />
+      </section>
+
+      <section className="right-section">
+        <DisplayArticle />
+      </section>
+    </div>
   );
 }
 
---
+++ client/src/components/displayArticle/DisplayArticle.tsx
@@ -0,0 +1,25 @@
+import "./displayArticle.css";
+
+interface displayArticle {
+  notion: string;
+  content: string;
+}
+
+export default function displayArticle() {
+  return (
+    <div className="right-section">
+      <button type="button" className="button-random">
+        {" "}
+        Generate random notion{" "}
+      </button>
+      <div className="container-article">
+        <h2> Provider - React </h2>
+      </div>
+      <p>
+        {" "}
+        Le provider c'est le robinet, le consummer (useContext) c'est le verre
+        d'eau{" "}
+      </p>
+    </div>
+  );
+}
